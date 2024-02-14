fd05f4c355c78e1d76af56036cd0338fefb1e08a
Nicolas Zanin <nicolas.zanin@etu.univ-cotedazur.fr>
2024-02-14 22:00:40 +0100

    task: déplacement méthode graphe

--- backend/ia/kingofescape.js
+++ backend/ia/kingofescape.js
@@ -6,26 +6,49 @@ let graph;
 // Classes
 
 class Graph {
-    /**
-     * Class constructor
-     */
     constructor() {
-        this.graph = new Map();
+        this.#initGraph();
+
     }
 
-    /**
-     * Add vertex to the graph
-     * @param vertex
-     */
+    #initGraph() {
+        this.graph = new Map();
+        let matrice = []
+
+        // create the matrice with coordinates x, y in string
+        for (let i = 0; i < 9; i++) {
+            matrice[i] = [];
+            for (let j = 0; j < 9; j++) {
+                matrice[i][j] = String((j + 1) * 10 + (9 - i));
+            }
+        }
+
+        // add all squares in the graph
+        for (let i = 0; i < 9; i++) {
+            for (let j = 0; j < 9; j++) {
+                this.addVertex(matrice[i][j]);
+            }
+        }
+
+        // Allow to give link between squares with orientation (right, left, up, down)
+        this.#addOrientationEdgeGraph(this.graph, matrice, 0, 1, 9, 9, 0, -1);
+        this.#addOrientationEdgeGraph(this.graph, matrice, 1, 0, 9, 9, -1, 0);
+        this.#addOrientationEdgeGraph(this.graph, matrice, 0, 0, 8, 9, 1, 0);
+        this.#addOrientationEdgeGraph(this.graph, matrice, 0, 0, 9, 8, 0, 1);
+    }
+
+    #addOrientationEdgeGraph(graph, matrice, startI, startJ, endI, endJ, gapI, gapJ) {
+        for (let i = startI; i < endI; i++) {
+            for (let j = startJ; j < endJ; j++) {
+                this.addEdge(matrice[i][j], matrice[i + gapI][j + gapJ]);
+            }
+        }
+    }
+
     addVertex(vertex) {
         this.graph.set(vertex, new Set());
     }
 
-    /**
-     * Add link between two vertex
-     * @param vertex first vertex
-     * @param node second vertex
-     */
     addEdge(vertex, node) {
         if (this.graph.has(vertex) && this.graph.has(node)) {
             this.graph.get(vertex).add(node);
@@ -33,14 +56,12 @@ class Graph {
         }
     }
 
-    /*verifyEdge(vertex, node) {
+    verifyEdge(vertex, node) {
         if (this.graph.has(vertex) && this.graph.has(node)) {
             return this.graph.get(vertex).has(node);
         }
         return false;
     }
-
-
 
     #verifyPossibilityToPlace(tuple1, tuple2, tuple3, tuple4, isVertical) {
         let boolVerify1 = this.verifyEdge(tuple1, tuple2);
@@ -58,110 +79,70 @@ class Graph {
         return false;
     }
 
-    addWall(wall, coordinatePlayer1, coordinatePlayer2) {
-        let tuple1 = wall.coordinate1.toNumber();
-        let tuple4 = wall.coordinate4.toNumber();
-        let tuple2 = Coordinate.toNumberXY(wall.coordinate1.x, wall.coordinate4.y);
-        let tuple3 = Coordinate.toNumberXY(wall.coordinate4.x, wall.coordinate1.y);
 
-        if (this.#verifyPossibilityToPlace(tuple1, tuple2, tuple3, tuple4, wall.isVertical)) {
+    // CoordinatePlayer2 can be undefined
+    isPossibleToPlaceWall(position, isVertical, positionIAPlayer, positionOtherPlayer) {
+        let numberPosition1 = Number.parseInt(position);
+        let position1 = position;
+        let position2 = String(numberPosition1 + 10);
+        let position3 = String(numberPosition1 + 1);
+        let position4 = String(numberPosition1 + 11);
+
+        if (this.#verifyPossibilityToPlace(position1, position2, position3, position4, isVertical)) {
             let mapTest = new Map(this.graph);
 
-            if (wall.isVertical) {
-                mapTest.get(tuple1).delete(tuple2);
-                mapTest.get(tuple2).delete(tuple1);
-                mapTest.get(tuple3).delete(tuple4);
-                mapTest.get(tuple4).delete(tuple3);
+            if (isVertical) {
+                this.deleteEdgeMap(mapTest, position1, position2);
+                this.deleteEdgeMap(mapTest, position3, position4);
             }
             else {
-                mapTest.get(tuple1).delete(tuple3);
-                mapTest.get(tuple3).delete(tuple1);
-                mapTest.get(tuple2).delete(tuple4);
-                mapTest.get(tuple4).delete(tuple2);
+                this.deleteEdgeMap(mapTest, position1, position3);
+                this.deleteEdgeMap(mapTest, position2, position4);
             }
 
-            if (verifyPossibilityWay(mapTest, coordinatePlayer1, true) &&
-                verifyPossibilityWay(mapTest, coordinatePlayer2, false)) {
-                this.graph = mapTest;
+            if (verifyPossibilityWay(mapTest, positionIAPlayer, aiPlay === 1) &&
+                (positionOtherPlayer !== undefined && verifyPossibilityWay(mapTest, positionOtherPlayer, false))) {
                 return true;
             }
+            return true;
         }
+    }
 
-        return false;
-    }*/
+    addWall(position, isVertical) {
+        let numberPosition1 = Number.parseInt(position);
+        let position1 = position;
+        let position2 = String(numberPosition1 + 10);
+        let position3 = String(numberPosition1 + 1);
+        let position4 = String(numberPosition1 + 11);
 
-    /**
-     * Delete link between two vertex
-     * @param vertex first vertex
-     * @param node second vertex
-     */
-    deleteEdge(vertex, node) {
-        if (this.graph.has(vertex) && this.graph.has(node)) {
-            this.graph.get(vertex).delete(node);
-            this.graph.get(node).delete(vertex);
+        if (isVertical) {
+            this.deleteEdgeMap(this.graph, position1, position2);
+            this.deleteEdgeMap(this.graph, position3, position4);
+        }
+        else {
+            this.deleteEdgeMap(this.graph, position1, position3);
+            this.deleteEdgeMap(this.graph, position2, position4);
+        }
+    }
+
+    deleteEdgeMap(map, vertex, node) {
+        if (map.has(vertex) && map.has(node)) {
+            map.get(vertex).delete(node);
+            map.get(node).delete(vertex);
         }
     }
 }
-
-/**
- * Initialize the graph
- * @returns {Graph} graph class initialized
- */
-function initGraph() {
-    let graph = new Graph();
-    let matrice = []
-
-    // create the matrice with coordinates x, y in string
-    for (let i = 0; i < 9; i++) {
-        matrice[i] = [];
-        for (let j = 0; j < 9; j++) {
-            matrice[i][j] = String((j + 1) * 10 + (9 - i));
-        }
-    }
-
-    // add all squares in the graph
-    for (let i = 0; i < 9; i++) {
-        for (let j = 0; j < 9; j++) {
-            graph.addVertex(matrice[i][j]);
-        }
-    }
-
-    // Allow to give link between squares with orientation (right, left, up, down)
-    addOrientationEdgeGraph(graph, matrice, 0, 1, 9, 9, 0, -1);
-    addOrientationEdgeGraph(graph, matrice, 1, 0, 9, 9, -1, 0);
-    addOrientationEdgeGraph(graph, matrice, 0, 0, 8, 9, 1, 0);
-    addOrientationEdgeGraph(graph, matrice, 0, 0, 9, 8, 0, 1);
-
-    return graph;
-}
-
-/**
- * Add all edges in the graph with the orientation
- * @param graph graph class
- * @param matrice matrice with coordinates x, y in string
- * @param startI start index i
- * @param startJ start index j
- * @param endI end index i
- * @param endJ end index j
- * @param gapI gap index i
- * @param gapJ gap index j
- */
-function addOrientationEdgeGraph(graph, matrice, startI, startJ, endI, endJ, gapI, gapJ) {
-    for (let i = startI; i < endI; i++) {
-        for (let j = startJ; j < endJ; j++) {
-            graph.addEdge(matrice[i][j], matrice[i + gapI][j + gapJ]);
-        }
-    }
-}
-
 
 // Private functions
+
+
 
 // Public functions
 
 function setup(AIplay) {
     aiPlay = AIplay;
-    graph = initGraph();
+    graph = new Graph();
+    console.log(graph);
     return '11';
 }
 
